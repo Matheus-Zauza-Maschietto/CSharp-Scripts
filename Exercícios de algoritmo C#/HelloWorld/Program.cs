64252ca1ed353b54aa8cf233aed3dbfc111b45cb
Matheus <matheus_maschietto@yahoo.com.br>
2022-09-14 10:57:25 -0300

    1° project

---
+++ Exercícios de algoritmo C#/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{}Press any key to exit...");
+            var key = Console.ReadKey(true);
+            Console.WriteLine(key.ToString());
+        }
+    }
+}
