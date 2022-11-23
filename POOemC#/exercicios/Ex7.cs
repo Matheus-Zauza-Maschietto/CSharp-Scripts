8a952b19ee9a493ada4adaa4117b55319d0693d0
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2022-11-23 15:54:44 -0300

    Update Ex7.cs

--- POOemC#/exercicios/Ex7.cs
+++ POOemC#/exercicios/Ex7.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace POOemC_.exercicios
+namespace POOemC_.exercicios.intermediario
 {
-    public class Ex7
-    {
+    class ContaBancaria{
+        private int _numeroConta;
+        public string Titular { get; set; }
+        public double Valor { get; set; }
+
+        public ContaBancaria(string titular, int _numero_conta){
+            _numeroConta = _numero_conta;
+            Titular = titular;
+        }
+        public ContaBancaria(string titular, int _numero_conta, double valor):this(titular, _numero_conta){
+            Valor = valor;
+        }
+
         
+
     }
 }
