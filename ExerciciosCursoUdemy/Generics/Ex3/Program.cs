ae790199bdfad288849213689524038219cea80f
matheus zauza maschietto <matheus_maschieto@yahoo.com.br>
2023-02-08 15:55:53 -0300

    update delegate

---
+++ ExerciciosCursoUdemy/Generics/Ex3/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+class Program
+{
+    public static void Main()
+    {
+        Dictionary<string, int> candidatos = new Dictionary<string, int>();
+
+        System.Console.Write("Enter fule full path: ");
+        string path = System.Console.ReadLine();
+
+        try{
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while(!sr.EndOfStream){
+                    string[] line = sr.ReadLine().Split(',');
+                    string name = line[0];
+                    int votes = int.Parse(line[1]);
+
+                    if(candidatos.ContainsKey(name)){
+                        candidatos[name] += votes;
+                    }
+                    else{
+                        candidatos.Add(name, votes);
+                    }
+                }
+            }
+
+            foreach(var item in candidatos){
+                System.Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }   
+        catch(IOException e){
+            System.Console.WriteLine($"erro: {e}");
+        }
+    }
+}
