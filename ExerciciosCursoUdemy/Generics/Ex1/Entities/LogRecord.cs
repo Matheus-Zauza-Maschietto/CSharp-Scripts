bb169febaa82dd781e959e7bcf3bd5556062ffb4
matheus zauza maschietto <matheus_maschieto@yahoo.com.br>
2023-02-08 09:42:36 -0300

    update novo desafio set

---
+++ ExerciciosCursoUdemy/Generics/Ex1/Entities/LogRecord.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Generics.Entities.Ex1
+{
+    public class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instante { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is LogRecord)){
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ ExerciciosCursoUdemy/Generics/Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Generics;
+using Generics.Entities.Ex1;
+class Program
+{
+    public static void Main()
+    {
+        HashCode<LogRecord> set = new HashSet<LogRecord>();
+
+        System.Console.Write("Enter file full path: ");
+        string path = System.Console.ReadLine();
+
+        try{
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while(!sr.EndOfStream){
+                    string[] line = sr.ReadLine().split(' ');
+                    string name = line[0];
+    	            DateTime instant = DateTime.Parse(line[1]);
+                    set.Add(new LogRecord {Username = name, Instante = instant});
+                }
+                System.Console.WriteLine("Total user: "+ set.Count);
+            }
+        }
+        catch(IOException e){
+            System.Console.WriteLine(e);
+        }
+    }
+}
